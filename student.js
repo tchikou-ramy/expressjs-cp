aa8404e89d77a859df90d96222cbeb8804225092
tchikou-ramy <ramytch10@gmail.com>
2022-12-07 15:20:01 -0900

    no changes

---
+++ student.js
@@ -0,0 +1,23 @@
+const student= [
+    {
+      id : 1 ,
+      name : "ramy" , 
+      age : 22
+    },
+    {
+        id : 2 ,
+        name : "X" , 
+        age : 45
+      },
+      {
+        id : 3 ,
+        name : "Y" , 
+        age : 45
+      },
+      {
+        id : 4 ,
+        name : "Z" , 
+        age : 45
+      }        
+]
+module.exports=student
